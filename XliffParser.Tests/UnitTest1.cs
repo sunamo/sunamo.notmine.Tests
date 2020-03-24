28e62f786500525617d747f36fa2f63a9066ae6b
Radek Jancik <ed@jepsano.net>
2020-03-24 22:56:57 +0100

    Mass commit 24-3-2020

---
+++ XliffParser.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml.XPath;
+
+namespace XliffParser.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void XlfXPath()
+        {
+            //var xd = new XlfDocument();
+
+            XmlNamespacesHolder x = new XmlNamespacesHolder();
+            var content = TF.ReadFile( @"D:\Documents\Visual Studio 2017\Projects\sunamo\sunamo\MultilingualResources\sunamo.en-US.xlf");
+            var xd = x.ParseAndRemoveNamespacesXDocument(content);
+
+            var nsmgr = x.nsmgr;
+
+            //var xd = XHelper.CreateXDocument();
+            //[original=@'WPF.TESTS/RESOURCES/EN-US.RESX']
+            var d = xd.XPathEvaluate("/xliff/file");
+            
+            var d2 = xd.XPathSelectElements("/xliff/file");
+            var d3 = xd.XPathSelectElements("/xliff");
+            int i = 0;
+        }
+    }
+}
