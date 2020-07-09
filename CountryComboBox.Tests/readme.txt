5f358b8eec262b1933b842739960ae98efd7aa5b
Radek Jančík <radek.jancik@sunamo.cz>
2020-07-09 21:34:35 +0200

    Mass commit 9-7-2020

---
+++ CountryComboBox.Tests/readme.txt
@@ -0,0 +1,2 @@
+﻿use CountryComboBox.Tests in Wpf.Tests
+must be there coz referencing many of project sunamo, shared,desktop etc.
---
+++ SlnGen.Common.Tests/MSBuildProjectLoader.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.Build.Utilities.ProjectCreation;
+using Xunit;
+
+namespace SlnGen.Common.Tests
+{
+    public class MSBuildProjectLoaderTests
+    {
+        [Fact]
+        public void LoadProjectReferencesTest()
+        {
+            MSBuildProjectLoader m = new MSBuildProjectLoader(BuildEngine.Create());
+            var p = new string [] { @"d:\Documents\Visual Studio 2017\Projects\sunamo\shared\shared.csproj" };
+            var pc = m.LoadProjectsAndReferences(p);
+            int i = 0;
+        }
+    }
+}
