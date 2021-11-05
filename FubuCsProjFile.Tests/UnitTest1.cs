215d7e81728fc847b0a682f8f9c3ce2bb07b8850
sunamo <radek.jancik@sunamo.cz>
2021-11-05 08:13:04 +0100

    Mass commit 5-11-2021

---
+++ FubuCsProjFile.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+using FubuCsProjFile.MSBuild;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace FubuCsProjFile.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            List<string> pr = Directory.GetFiles(@"d:\_Test\sunamo.notmine\FubuCsProjFile\FrameworkNameDetector\").ToList() ;
+
+            foreach (var item in pr)
+            {
+                
+                var msb = MSBuildProject.LoadFrom(item);
+                var result = FrameworkNameDetector.Detect(msb);
+                Debug.WriteLine(result);
+            }
+
+
+        }
+    }
+}
