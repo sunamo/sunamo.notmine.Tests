a931186525b4ec563bc5445df87d002f8adb85f5
sunamo <radek.jancik@sunamo.cz>
2021-11-29 18:05:10 +0100

    Mass commit 29-11-2021

---
+++ TurnerSoftware.SitemapTools.Tests/Parser/TextSitemapParserTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using NUnit.Framework;
+using TurnerSoftware.SitemapTools.Parser;
+
+public class TextSitemapParserTests
+{
+    [Test]
+    public void ParseSitemapTest()
+    {
+        var reader = new StreamReader(new FileStream(@"e:\Documents\vs\Projects\sunamo.cz\lyrics.sunamo.cz\sitemap.xml", FileMode.Open));
+        TextSitemapParser t = new TextSitemapParser();
+        var file = t.ParseSitemapXml((TextReader)reader);
+
+        var i =0;
+    }
+}
